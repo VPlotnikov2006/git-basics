a2d9484a046f01c7b3bd359b5218acf11caae86e
Plotnikov VD <plotnikov.vd@students.sch2.ru>
2022-09-16 17:10:03 +0300

    Inputfile

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	string name, surname, film;
-	cin >> name >> surname >> film;
+	vector<string> answers;
+	ifstream questions("inputfile");
+	if(!questions)
+        return 2;
+    string question;
+    while (getline(questions, question)) {
+        cout << question << "\n";
+        string ans;
+        getline(cin, ans);
+        answers.push_back(ans);
+    }
 	ofstream out("results.txt", ios_base::app);
 	if (!out)
         return 1;
-	out << name << " " << surname << " " << film << "\n";
+	for (auto x: answers)
+        out << x << "\n";
+    out << "\n";
 	out.close();
 	return 0;
 }
